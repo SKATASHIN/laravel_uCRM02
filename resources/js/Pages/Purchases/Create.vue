7d0b44aece89494aeca9fb04f9823ffcfb96e6b7
SKATASHIN <katashin0544@gmail.com>
2022-10-06 19:08:26 +0900

    AxiosError

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -125,7 +125,7 @@ const form = reactive({
                             </div>
 
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                  <div class="">
                                     <label for="price" class="leading-7 text-sm text-gray-600">合計金額</label><br>
                                     <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                       合計 {{ totalPrice }} 円
